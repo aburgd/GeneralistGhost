0fbe8de334c1a48ae1bcb2cf5565a07bace1a2ee
Alec Burgdorf <aeburgd@gmail.com>
2021-04-14 14:03:01 +0000

    fix(kick)

--- src/commands/moderation/kick.ts
+++ src/commands/moderation/kick.ts
@@ -1,24 +1,16 @@
 import { Client, Command, CommandoMessage } from 'discord.js-commando'
-import { GuildMember } from 'discord.js'
+import { GuildMember, TextChannel } from 'discord.js'
 
 interface IKickOptions {
   member: GuildMember
-  reason?: string
+  reason: string
 }
 
 module.exports = class KickCommand extends Command {
-  async run (message: CommandoMessage, { member, reason }: IKickOptions): Promise<CommandoMessage> {
-    if (message.channel.id !== '831159533132316682') return await message.say('You can\'t run that here.')
-    const memberToKick: GuildMember = member
-    const reasonString: string = reason?.toString() ?? ''
-    await memberToKick.kick(reasonString)
-    return await message.say(`${memberToKick.user.toString()} has been kicked with reason: ${reasonString}`)
-  }
-
   constructor (client: Client) {
     super(client, {
       name: 'kick',
-      aliases: ['boot'],
+      aliases: ['boot', 'yeet'],
       group: 'moderation',
       memberName: 'kick',
       description: 'Kicks a member',
@@ -35,5 +27,22 @@ module.exports = class KickCommand extends Command {
         }
       ]
     })
+  }
+
+  async run (message: CommandoMessage, { member, reason }: IKickOptions): Promise<CommandoMessage> {
+    // only in set channel
+    if (message.channel.id !== '831164820526071889') return await message.say('You can\'t run that here.')
+    // make member a GuildMember
+    const memberToKick: GuildMember = member
+    // make reason a string
+    const reasonString: string = reason.toString()
+    console.log(memberToKick.user.username)
+    console.log(reasonString)
+    const kickConfirm: string = `${memberToKick.user.username} has been kicked with reason: ${reasonString}`
+    const modChannel: TextChannel = memberToKick.guild.channels.cache.find(ch => ch.id === '831159533132316682') as TextChannel
+    modChannel.send(kickConfirm).catch(console.error)
+    // kick with reasonString as reason
+    await memberToKick.kick(reasonString).catch(console.error)
+    return await message.say(`${memberToKick.user.toString()} has been kicked with reason: ${reasonString}`)
   }
 }
