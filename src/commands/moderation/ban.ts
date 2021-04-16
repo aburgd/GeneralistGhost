00e045f1e5ece6595b284cf99143e05c60406559
Alec Burgdorf <aeburgd@gmail.com>
2021-04-16 14:14:37 +0000

    add(days arg)

--- src/commands/moderation/ban.ts
+++ src/commands/moderation/ban.ts
@@ -4,6 +4,7 @@ import { GuildMember, TextChannel } from 'discord.js'
 interface IBanOptions {
     member: GuildMember
     reason: string
+    days: number
 }
 
 module.exports = class BanCommand extends Command {
@@ -24,18 +25,23 @@ module.exports = class BanCommand extends Command {
           key: 'reason',
           prompt: 'What is the reason for banning?',
           type: 'string'
+        },
+        {
+          key: 'days',
+          prompt: 'How many days of messages to delete?',
+          type: 'integer'
         }
       ]
     })
   }
 
-  async run (message: CommandoMessage, { member, reason }: IBanOptions): Promise<CommandoMessage> {
+  async run (message: CommandoMessage, { member, reason, days }: IBanOptions): Promise<CommandoMessage> {
     if (message.channel.id !== '831164820526071889') return await message.say('You can\'t run that here.')
     const memberToBan: GuildMember = member
     const reasonString: string = reason.toString()
     const kickConfirm: string = `${memberToBan.user.username} has been banned with reason ${reasonString}`
     const modChannel: TextChannel = memberToBan.guild.channels.cache.find(ch => ch.id === '831159533132316682') as TextChannel
-    await memberToBan.ban({ reason: reasonString }).catch(console.error)
+    await memberToBan.ban({ days: days as number, reason: reasonString }).catch(console.error)
     await modChannel.send(kickConfirm).catch(console.error)
     return await message.say(kickConfirm)
   }
